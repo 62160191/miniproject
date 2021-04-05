62bcc1bc9035131302186fa62df8ecf6e5c188d2
62160191 <62160191@go.buu.ac.th>
2021-04-05 20:17:46 +0700

    'miniproject'

---
+++ Miniproject/blog/db.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2021 at 06:38 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `exe`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
+CREATE TABLE `article` (
+  `id` int(20) NOT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `head_img` varchar(100) DEFAULT '',
+  `descript` mediumtext DEFAULT NULL,
+  `status` tinyint(1) DEFAULT 0 COMMENT '1=public,2=private',
+  `created` date DEFAULT curdate(),
+  `view` int(10) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`id`, `name`, `head_img`, `descript`, `status`, `created`, `view`) VALUES
+(1, 'addd', '', 'wow zxa', 0, '2021-03-25', 0),
+(2, 'นี่คือสุดยอดของบทความที่น่าทึ่งที่สุดในโลก', '', 'ว้าวซ่าโอ้มายก็อด ว้าวๆๆ นี่ๆๆๆ', 0, '2021-03-25', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
